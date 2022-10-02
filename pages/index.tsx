5c737f8714e88f59bcf9f23e22dcb0d564ffd6ef
Robin <git+r@obin.ch>
2022-10-02 20:22:31 +0000

    chore: configure emotion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
+import { css } from '@emotion/react'
 import type { NextPage } from 'next'
+
+const HomeStyles = css`
+  background-color: red;
+`
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div css={HomeStyles}>
       Hello
     </div>
   )
