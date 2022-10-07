b76a36cbf62a215b094f061df8c22bfe8ed1f83b
Robin <git+r@obin.ch>
2022-10-07 14:14:33 +0000

    chore: add brand

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -1,5 +1,11 @@
+import { css } from "@emotion/react";
 import { FormattedMessage } from "react-intl";
 
+const brandStyles = css`
+  margin: 0;
+  line-height: 1;
+`;
+
 export default function Brand() {
-  return <h1><FormattedMessage id="brand" /></h1>;
+  return <h1 css={brandStyles}><FormattedMessage id="brand" /></h1>;
 }
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,17 @@
+import { css } from "@emotion/react";
 import { AppShell, Header, Navbar } from "@mantine/core";
 import { PropsWithChildren } from "react";
 import Brand from "./Brand";
 
 type LayoutProps = PropsWithChildren<{}>;
 
+const headerStyles = css`
+  display: flex;
+  align-items: center;
+`
+
 export default function Layout({children}: LayoutProps) {
-  const header = <Header height={60}><Brand /></Header>
-  const navbar = <Navbar width={{base: 300}}>Navbar</Navbar>
+  const header = <Header height={60} css={headerStyles} p="xs"><Brand /></Header>
+  const navbar = <Navbar width={{base: 300}} p="xs">Navbar</Navbar>
   return <AppShell header={header} navbar={navbar}>{children}</AppShell>
 }
