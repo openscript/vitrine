9659e951a0b7fbc143239aee6707d3f8603763bd
Robin <git+r@obin.ch>
2022-10-08 01:45:30 +0000

    chore: translate login form

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -3,6 +3,7 @@ import { Button, Center, Paper, PasswordInput, TextInput } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { IconLock, IconMail } from '@tabler/icons';
 import { ReactElement } from 'react';
+import { FormattedMessage, useIntl } from 'react-intl';
 import EmptyLayout from '../components/EmptyLayout';
 import { NextPageWithLayout } from './_app';
 
@@ -15,6 +16,7 @@ const LoginPaperStyles = css`
 `;
 
 const Login: NextPageWithLayout = () => {
+  const intl = useIntl();
   const form = useForm({
     initialValues: {
       email: '',
@@ -30,13 +32,21 @@ const Login: NextPageWithLayout = () => {
         <form onSubmit={form.onSubmit(handleSubmit)}>
           <TextInput
             required
-            placeholder="Bspw.: example@example.com"
-            label="E-Mail"
+            placeholder={intl.formatMessage({ id: 'form.email.placeholder' })}
+            label={intl.formatMessage({ id: 'form.email.label' })}
             {...form.getInputProps('email')}
             icon={<IconMail size={20} stroke={1.5} />}
           />
-          <PasswordInput required label="Passwort" {...form.getInputProps('password')} icon={<IconLock size={20} stroke={1.5} />} />
-          <Button type="submit">Anmelden</Button>
+          <PasswordInput
+            required
+            placeholder={intl.formatMessage({ id: 'form.password.placeholder' })}
+            label={intl.formatMessage({ id: 'form.password.label' })}
+            {...form.getInputProps('password')}
+            icon={<IconLock size={20} stroke={1.5} />}
+          />
+          <Button type="submit">
+            <FormattedMessage id="form.actions.login" />
+          </Button>
         </form>
       </Paper>
     </Center>
