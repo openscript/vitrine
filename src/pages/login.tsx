a0e1336c2b61b5c2d5f7638a901dfa377019270b
Robin <git+r@obin.ch>
2022-10-08 01:34:41 +0000

    chore: create login form

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,7 @@
 import { css } from '@emotion/react';
-import { Center, Paper } from '@mantine/core';
+import { Button, Center, Paper, PasswordInput, TextInput } from '@mantine/core';
+import { useForm } from '@mantine/form';
+import { IconLock, IconMail } from '@tabler/icons';
 import { ReactElement } from 'react';
 import EmptyLayout from '../components/EmptyLayout';
 import { NextPageWithLayout } from './_app';
@@ -8,10 +10,35 @@ const LoginStyles = css`
   height: 100%;
 `;
 
+const LoginPaperStyles = css`
+  min-width: 24rem;
+`;
+
 const Login: NextPageWithLayout = () => {
+  const form = useForm({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const handleSubmit = () => { };
+
   return (
     <Center css={LoginStyles}>
-      <Paper withBorder>Login</Paper>
+      <Paper withBorder p="xs" shadow="sm" css={LoginPaperStyles}>
+        <form onSubmit={form.onSubmit(handleSubmit)}>
+          <TextInput
+            required
+            placeholder="Bspw.: example@example.com"
+            label="E-Mail"
+            {...form.getInputProps('email')}
+            icon={<IconMail size={20} stroke={1.5} />}
+          />
+          <PasswordInput required label="Passwort" {...form.getInputProps('password')} icon={<IconLock size={20} stroke={1.5} />} />
+          <Button type="submit">Anmelden</Button>
+        </form>
+      </Paper>
     </Center>
   );
 };
