275748986189c0c84ecb271541fec3ed8fb398cf
Robin <git+r@obin.ch>
2022-10-10 06:32:32 +0000

    chore: refactor guards

--- src/components/guards/AuthGuard.tsx
+++ src/components/guards/AuthGuard.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 import { Fragment, PropsWithChildren, useEffect, useState } from 'react';
-import { supabase } from '../utils/supabaseClient';
+import { supabase } from '../../utils/supabaseClient';
 
 type AuthGuardProps = PropsWithChildren<{
   isUnauthenticatedGuard?: boolean;
