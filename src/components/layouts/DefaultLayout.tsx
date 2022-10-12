fc0a6f7cb5ab5a2c10915e5bbe5dcbb273824ca3
Robin <git+r@obin.ch>
2022-10-12 17:01:56 +0000

    fix: redirect to home on logout

--- src/components/layouts/DefaultLayout.tsx
+++ src/components/layouts/DefaultLayout.tsx
@@ -1,6 +1,8 @@
 import { AppShell } from '@mantine/core';
+import { useRouter } from 'next/router';
 import { PropsWithChildren, useEffect } from 'react';
 import shallow from 'zustand/shallow';
+import { CONFIGURATION } from '../../configuration';
 import { useStore } from '../../state/store';
 import { supabase } from '../../utils/supabaseClient';
 import DefaultHeader from './default/DefaultHeader';
@@ -10,8 +12,14 @@ type DefaultLayoutProps = PropsWithChildren<{}>;
 
 export default function DefaultLayout({ children }: DefaultLayoutProps) {
   const [fetchProfile, avatar, session] = useStore((state) => [state.fetchProfile, state.avatar, state.session], shallow);
+  const router = useRouter();
   const isAuthenticated = session ? true : false;
-  const handleSignOut = () => supabase.auth.signOut();
+  const handleSignOut = async () => {
+    const { error } = await supabase.auth.signOut();
+    if (!error) {
+      router.push(CONFIGURATION.PATHS.HOME);
+    }
+  };
 
   useEffect(() => {
     if (isAuthenticated) {
