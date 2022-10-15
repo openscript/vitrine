8c3dcdc232080cabc6eacd167dcabd6adf5f89b5
Robin <git+r@obin.ch>
2022-10-15 08:30:33 +0000

    fix: update avatar

--- supabase/migrations/20221011133921_add_profiles.sql
+++ supabase/migrations/20221011133921_add_profiles.sql
@@ -45,3 +45,7 @@ CREATE POLICY "Avatar images are publicly accessible."
 CREATE POLICY "Anyone can upload an avatar."
   ON storage.objects FOR INSERT
   WITH CHECK ( bucket_id = 'avatars' );
+
+CREATE POLICY "Anyone can update an avatar."
+  ON storage.objects FOR UPDATE
+  USING ( bucket_id = 'avatars' );
